e399a93edd2adf865756681416c898618710c7aa
Devexer <me@d3v.no>
2016-12-30 10:48:22 +0100

    More work on header

--- resources/views/partials/global/header.blade.php
+++ resources/views/partials/global/header.blade.php
@@ -2,7 +2,7 @@
     <span>TaskList</span>
     <div class="user-section">
         <img src="http://placehold.it/20x20">
-        <span>Joachim Dalen</span>
+        <span>JD</span>
         <i class="fa fa-arrow-circle-down" id="profileMenu"></i>
     </div>
 </div>
